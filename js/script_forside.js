5b489ab5d38046b538356e17259a7e4c8a67134e
VickiOx <viox0001@stud.kea.dk>
2023-05-22 17:47:32 +0200

    add hamburgermenu forside

--- js/script_forside.js
+++ js/script_forside.js
@@ -18,3 +18,19 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+/*-----------------------------------burgermenu-----------------------*/
+window.onload = function (){
+
+  const menu_btn = document.querySelector('.hamburger');
+  const mobil_menu = document.querySelector('.mobil-nav');
+
+  menu_btn.addEventListener('click', function () {
+
+  menu_btn.classList.toggle('is-active');
+  mobil_menu.classList.toggle('is-active');
+
+});
+
+}
